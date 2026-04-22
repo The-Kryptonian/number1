9e19330b47fd61951df1dd046c2f7439b10b4551
The-Kryptonian <kryptonianencryption@gmail.com>
2026-04-22 20:32:11 +0200

    Update page.tsx

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -22,7 +22,7 @@ const Welcome = () => {
  return (
     <div style = {{backgroundColor: 'white',  minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingLeft: 20, paddingRight: 20}}>
         
-        <div className={berlinSans.className} style={{border: '1px solid black', borderRadius: 15,width: '94%', height: 55, fontSize: 25, display:'flex', justifyContent: 'center', alignItems: 'center', marginTop: 30, textShadow: '3px 3px 3px rgb(208, 208, 208)'}}>Z I C R E X</div>
+        <div className={berlinSans.className} style={{border: '1px solid black', borderRadius: 15,width: '90%', height: 55, fontSize: 25, display:'flex', justifyContent: 'center', alignItems: 'center', marginTop: 30, textShadow: '3px 3px 3px rgb(208, 208, 208)'}}>Z I C R E X</div>
 
         <img src={welcomeImage} alt="Welcoming Image" width='350px' style={{marginTop: 70}}/>
 
@@ -33,8 +33,8 @@ const Welcome = () => {
         <div className={arialNarrow.className} style={{fontSize: 20, marginBottom: 40}}>Refuel your wallet with zero friction, keep your portfolio moving with just a few taps.</div>
 
         <div style={{display: 'flex', flexDirection: 'row', gap: 40,}}>
-            <button onClick={()=>{router.push('/')}} style={{padding: 5, width: 120, borderRadius: 5, fontSize: 20, fontWeight: 650, border: "2px solid rgb(226, 226, 226)", boxShadow: '3px 3px 3px rgb(220, 220, 220)'}}> HOME </button>
-            <button onClick={()=>{router.push('/login')}} style={{backgroundColor: 'rgb(0, 172, 240)', padding: 5, width: 120, borderRadius: 5, fontSize: 20, fontWeight: 650, color: 'rgb(255, 248, 248)'}}> Sign In </button>
+            <button onClick={()=>{router.push('/')}} style={{padding: 5, width: 130, borderRadius: 5, fontSize: 20, fontWeight: 650, border: "2px solid rgb(226, 226, 226)", boxShadow: '3px 3px 3px rgb(220, 220, 220)'}}> HOME </button>
+            <button onClick={()=>{router.push('/login')}} style={{backgroundColor: 'rgb(0, 172, 240)', padding: 5, width: 130, borderRadius: 5, fontSize: 20, fontWeight: 650, color: 'rgb(255, 248, 248)'}}> Sign In </button>
         </div>
 
     </div>
