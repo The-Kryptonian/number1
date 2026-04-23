8d7f071df115e6348233eb0727a9574b44643193
The-Kryptonian <kryptonianencryption@gmail.com>
2026-04-23 17:47:09 +0200

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -24,7 +24,7 @@ const router = useRouter()
         <div style={{position:'relative', top: 10}}>   
 
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 30}}>
-            <div style={{width:90, height: 90, borderRadius: 45, backgroundColor: 'rgb(201, 201, 201)'}}></div>
+            <div className={berlinSans.className} style={{border: '1px solid black', borderRadius: 15,width: '90%', height: 55, fontSize: 25, display:'flex', justifyContent: 'center', alignItems: 'center', textShadow: '3px 3px 3px rgb(208, 208, 208)'}}>Z I C R E X</div>
             <div style={{fontSize:35, fontWeight: 550, marginTop: 15, marginBottom: 0}} className={berlinSans.className}>Create Your Account!</div>
             <div className={arialNarrow.className} style={{fontSize: 15, marginBottom: 40}}>Create an account to discover more features made just for you</div>
         </div>  
